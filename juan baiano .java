53c20bc41937e665fc05f6f89f2e811ba26fec3a
juannascimento2007 <juan.nascimento.santos@escola.pr.gov.br>
2024-09-04 16:07:32 -0300

    Add files via upload

---
+++ juan baiano .java
@@ -0,0 +1,87 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Qual eo pricipal objetivo da adubação na agricultura",
+                afirmacao: "fornecer nutrientes esseciais para planta. "
+            },
+            {
+                texto: ""Qual eo pricipal objetivo da adubação na agricultura,
+                afirmacao: "aumento a salinidade do solo."
+            }
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "O que e a pratica do plantio direto na agricultura.",
+                afirmacao: "metodo que consiste em plantar as sememtes sem revolver o solo."
+            },
+            {
+                texto: ".O que e a pratica do plantio direto na agricultura",
+                afirmacao: "metodo que envolve o constante revolvimento do solo para areja-lo."
+            }
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Qual e a importamcia da polinização na agricultura.",
+                afirmacao: "diminuir a divercidades genetica das culturas."
+            },
+            {
+                texto: ".Qual e a importamcia da polinização na agricultura",
+                afirmacao: "fornecer a reprodução das plantas e garatil a produção de frutos e sementes."
+            }
+        ]
+    },
+       
+        ]
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
